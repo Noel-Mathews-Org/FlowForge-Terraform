8da1df76c5d08ccbc20322a0d7cb3f0b624c4e33
noelmc-ust <Noel.Mathews@ust.com>
2026-06-12 01:57:04 +0530

    made changes in docs

--- modules/aks/outputs.tf
+++ modules/aks/outputs.tf
@@ -5,5 +5,5 @@ output "kubelet_identity_object_id" {
   value = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
 }
 output "aks_managed_identity_principal_id" {
-  value = azurerm_kubernetes_cluster.aks.identity[0].principal_id
+  value = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
 }
